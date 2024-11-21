47ba7d87833475922292b9d20322d67b92dad924
Anonymous <yousef.hosseini@gmail.com>
2024-11-21 22:11:01 +1100

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ fastify.all('/incoming-call', async (request, reply) => {
     const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
                           <Response>
                               <Say>Hi,  I AI</Say>
-                              <Pause length="0.1"/>
+                              <Pause length="1"/>
                               <Say>How can I help?</Say>
                               <Connect>
                                   <Stream url="wss://${request.headers.host}/media-stream" />
