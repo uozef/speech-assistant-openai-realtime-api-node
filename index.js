0520378d1c5890488f429d25bc4f6a40b05397d8
Anonymous <yousef.hosseini@gmail.com>
2024-11-21 22:02:05 +1100

    Update index.js

--- index.js
+++ index.js
@@ -50,9 +50,7 @@ fastify.get('/', async (request, reply) => {
 fastify.all('/incoming-call', async (request, reply) => {
     const twimlResponse = `<?xml version="1.0" encoding="UTF-8"?>
                           <Response>
-                              <Say>Please wait while we connect your call to the A. I. voice assistant, powered by Twilio and the Open-A.I. Realtime API</Say>
-                              <Pause length="1"/>
-                              <Say>O.K. you can start talking!</Say>
+                           
                               <Connect>
                                   <Stream url="wss://${request.headers.host}/media-stream" />
                               </Connect>
